c9a725150dc820399777e5846bbd04bc76cc14d7
David Karam <david@posttool.com>
2015-05-25 09:32:36 -0700

    calendar cleanup, show months, show sat/sun

--- app/public/js/events.js
+++ app/public/js/events.js
@@ -23,11 +23,13 @@ $next.click(function () {
   draw_cal_for()
 });
 
+var now = moment().subtract(1, 'day');
+var evtidx = {};
+
 function draw_cal_for() {
-  var now = moment().add(1, 'day');
   $sel.val(d.format('MMMM'));
   $.ajax('/events?d=' + d.format('YYYY-MM-DD')).done(function (events) {
-    var evtidx = index_events(events);
+    index_events(events);
     var d0 = moment(d);
     d0.startOf('week');
     var $cal = $("#calendar");
@@ -36,55 +38,15 @@ function draw_cal_for() {
       var $row = $("<div class='row'></div>");
       $cal.append($row);
       for (var j = 0; j < 7; j++) {
-        if (j != 0 && j != 6) {
-          (function (day, before) {
-            var $c = $("<div class='four columns event'></div>");
-            if (before)
-              $c.addClass('before');
-            var $d = $("<div class='event_header'></div>");
-            $d.append(d0.format("ddd DD"));
-            $c.append($d);
-            var id = d0.format('MM-DD');
-            if (evtidx[id]) {
-              evtidx[id].forEach(function (event) {
-                var $d = $("<div class='event_item'></div>");
-//                $d.append(event.leader.contact.name);
-                $d.append(moment(event.when).format("h:mm a") + " " + event.location)
-                if (before)
-                  $d.addClass('before');
-                if (event.spaces <= event.kids.length)
-                  $d.addClass('full');
-                $d.click(function () {
-                  location.href = '/event/' + event._id;
-                });
-                $c.append($d);
-                console.log(event)
-              });
-            }
-            if (!before) {
-              var $add = $("<div class='event_add'><i class='fa fa-plus'></i>  Add</div>");
-              $add.hide();
-              $c.append($add);
-              $c.mouseover(function () {
-                $add.show();
-              });
-              $c.mouseout(function () {
-                $add.hide();
-              });
-              $add.click(function () {
-                location.href = "/event/create?d=" + day.format('YYYY-MM-DD')
-              })
-            }
-            $row.append($c);
-          })(moment(d0), d0.isBefore(now));
-        }
+        var $c = get_calendar_item(moment(d0));
+        $row.append($c);
         d0.add(1, 'day');
       }
     }
   })
 }
 function index_events(events) {
-  var evtidx = {};
+  evtidx = {};
   events.forEach(function (e) {
     var id = moment(e.when).format('MM-DD');
     if (evtidx[id])
@@ -93,4 +55,50 @@ function index_events(events) {
       evtidx[id] = [e];
   });
   return evtidx;
+}
+function get_calendar_item(day) {
+  var before = day.isBefore(now);
+  var $c = $("<div class='four columns event'></div>");
+  if (before)
+    $c.addClass('before');
+  var $d = $("<div class='event_header'></div>");
+  if (day.date() == 1) {
+    $d.append(day.format("MMMM D"));
+    $c.addClass('first-day-of-month');
+  }
+  else
+    $d.append(day.format("ddd D"));
+  $c.append($d);
+  var id = day.format('MM-DD');
+  if (evtidx[id]) {
+    evtidx[id].forEach(function (event) {
+      var $d = $("<div class='event_item'></div>");
+      //$d.append(event.leader.contact.name);
+      $d.append(moment(event.when).format("h:mm a") + " " + event.location)
+      if (before)
+        $d.addClass('before');
+      if (event.spaces <= event.kids.length)
+        $d.addClass('full');
+      $d.click(function () {
+        location.href = '/event/' + event._id;
+      });
+      $c.append($d);
+      console.log(event)
+    });
+  }
+  if (!before) {
+    var $add = $("<div class='event_add'><i class='fa fa-plus'></i>  Add</div>");
+    $add.hide();
+    $c.append($add);
+    $c.mouseover(function () {
+      $add.show();
+    });
+    $c.mouseout(function () {
+      $add.hide();
+    });
+    $add.click(function () {
+      location.href = "/event/create?d=" + day.format('YYYY-MM-DD')
+    })
+  }
+  return $c;
 }
